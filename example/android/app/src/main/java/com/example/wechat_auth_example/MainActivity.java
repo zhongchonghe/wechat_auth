ad180311966162474afbb48b4eda80ecbfbc017a
zhongchonghe <46522160+zhongchonghe@users.noreply.github.com>
2022-08-14 16:11:17 +0800

    Update MainActivity.java

--- example/android/app/src/main/java/com/example/wechat_auth_example/MainActivity.java
+++ example/android/app/src/main/java/com/example/wechat_auth_example/MainActivity.java
@@ -1,6 +1,16 @@
 package com.example.wechat_auth_example;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+
+    @Override
+    protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(FlutterActivity.createDefaultIntent(this));
+    }
 }
